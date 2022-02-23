0e997cf53c46d696195f6420064de71c688b70c5
CamilaCF22 <20204002@esen.edu.sv>
2022-02-22 21:08:02 -0600

    Status

--- db/dbstatus.py
+++ db/dbstatus.py
@@ -3,20 +3,46 @@ from models.status import Status
 
 
 def selectAllStatus():
-    pass
+    connection = createConnectionDB()
+    with connection:
+        with connection.cursor() as cursor:
+            sql = "SELECT * FROM specialtodolistdb.status;"
+            cursor.execute(sql)
+            result = cursor.fetchall()
+            return result
 
 
 def insertNewStatus(status: Status):
-    pass
+    connection = createConnectionDB()
+    with connection:
+        with connection.cursor() as cursor:
+            sql = "INSERT INTO `specialtodolistdb`.`status` (`id`,`name`)VALUES (%s, %s);"
+            cursor.execute(sql, (0, person.name))
+        connection.commit()
 
 
 def selectStatusById(id) -> dict:
-    pass
+    connection = createConnectionDB()
+    with connection:
+        with connection.cursor() as cursor:
+            sql = "SELECT * FROM specialtodolistdb.status where id=%s;"
+            cursor.execute(sql, id)
+            result = cursor.fetchone()
+            return result
 
 
 def updateStatus(status: Status):
-    pass
-
+    connection = createConnectionDB()
+    with connection:
+        with connection.cursor() as cursor:
+            sql = "UPDATE `specialtodolistdb`.`statys` SET `name` = %s WHERE `id` = %s;"
+            cursor.execute(sql, (person.name, person.id))
+        connection.commit()
 
 def deleteStatus(id: int):
-    pass
+    connection = createConnectionDB()
+    with connection:
+        with connection.cursor() as cursor:
+            sql = "DELETE FROM `specialtodolistdb`.`status` WHERE id=%s;"
+            cursor.execute(sql, id)
+        connection.commit()
